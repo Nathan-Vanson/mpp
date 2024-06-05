79a5b501952dc1cf17f3b9ea3ac75421d0b35216
Anthony Christoforou <christoforouanthony@gmail.com>
2024-06-05 17:58:55 +0200

    backup 06/05/24 17:58:55

--- mpp/models/gradient_boosting.py
+++ mpp/models/gradient_boosting.py
@@ -2,8 +2,8 @@ from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error
 
 class GradientBoostingModel:
-    def __init__(self, n_estimators=100, learning_rate=0.1):
-        self.model = GradientBoostingRegressor(n_estimators=n_estimators, learning_rate=learning_rate)
+    def __init__(self, n_estimators=100, learning_rate=0.1, max_depth=3):
+        self.model = GradientBoostingRegressor(n_estimators=n_estimators, learning_rate=learning_rate, max_depth=max_depth)
 
     def train(self, X_train, y_train):
         self.model.fit(X_train, y_train)
--- mpp/models/random_forest.py
+++ mpp/models/random_forest.py
@@ -2,8 +2,8 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
 class RandomForestModel:
-    def __init__(self, n_estimators=100):
-        self.model = RandomForestRegressor(n_estimators=n_estimators)
+    def __init__(self, n_estimators=100, max_depth=5):
+        self.model = RandomForestRegressor(n_estimators=n_estimators, max_depth=5)
 
     def train(self, X_train, y_train):
         self.model.fit(X_train, y_train)
